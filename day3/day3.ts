bdeda5132ee0c1ef70d5d749cafc54c5f17f3cac
Kevin Batdorf <kbat82@gmail.com>
2021-12-04 00:52:41 -0500

    day three

--- day3/day3.ts
+++ day3/day3.ts
@@ -19,3 +19,36 @@ const epsilon = [...gamma]
 
 // Convert to decimal and multiply
 console.log('part1', parseInt(gamma, 2) * parseInt(epsilon, 2))
+
+// I hated this one
+const partB = (position = 1, unwantedKeys: Set<string> = new Set()) => {
+    return input[0].split('')
+    // Rotate the array so each row is now what was previously a column
+    .map((_val, index) => input.map(row => parseInt(row[index], 10)))
+    .reduce((input, current, index) => {
+        // If we have only one number left, bail early (or just return continuously)
+        if (Object.entries(input).length === 1) {
+            return input
+        }
+        // Remove values that we previously determined were unwanted
+        const currentTotal = current.filter((_, i) => !unwantedKeys.has(i.toString()))
+        // Are we looking for a 1 or a 0 ?
+        const wanted = (currentTotal.reduce((t, n) => {return t + n}, 0) / currentTotal.length) >= 0.5 ? position : 1 - position
+
+        // Loop over each value and look for the numbers with the value we do not want
+        Object.keys(input).filter(number => {
+            return parseInt(input[parseInt(number, 10)].split('')[index], 10) !== wanted
+            // Add unwanted values to keep track for later
+        }).forEach(v => unwantedKeys.add(v))
+
+        // Delete the numbers from the input set
+        unwantedKeys.forEach(k => delete input[parseInt(k, 10)])
+        return input
+    }, {...input})
+}
+
+const oxygenGeneratorRating = partB(1)
+const CO2ScrubberRating = partB(0)
+// Convert to decimal and multiply
+const total = parseInt(Object.values(oxygenGeneratorRating)[0].toString(), 2) * parseInt(Object.values(CO2ScrubberRating)[0].toString(), 2)
+console.log('part2', total)
